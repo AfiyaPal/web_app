4677fe380af1101dc04d2c858862e6bded8dbf6c
bedan <bedanmacharia13@gmail.com>
2026-02-12 16:24:06 +0300

    Added the landing page

--- src/settings/base_settings.py
+++ src/settings/base_settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Media files
 
@@ -126,6 +126,6 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'frontend:home'
 LOGOUT_REDIRECT_URL = 'users:login'
 
-DEBUG = False # Keep False for production
+DEBUG = True # Keep False for production
 ALLOWED_HOSTS = ['afiyapal.pythonanywhere.com', '127.0.0.1', 'localhost']
 SECRET_KEY = os.environ.get('SECRET_KEY', 'AIzaSyBSDobDoFGijBDhQ2vu7QR3Q6thy50QtIE')
